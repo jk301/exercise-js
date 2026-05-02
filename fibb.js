b7e2bc29d944d6d88533c8415afa57a98dc9b98e
junaid <jk7948677@gmail.com>
2026-05-02 21:26:30 +0530

    fib normal

--- fibb.js
+++ fibb.js
@@ -1,3 +1,11 @@
 function fib_normal (num) {
-    
+    const array = [0, 1]
+    if (num === 1) return [array[0]]
+    console.log("hello")
+    for (let i = 0; i < num - 2; i++) {
+        array.push(array[i] + array[i + 1])
+    }
+    return array
 }
+
+console.log(fib_normal(8))
